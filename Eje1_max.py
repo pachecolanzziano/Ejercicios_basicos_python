d751dc24d80afe3f302088fb26710b18da506e72
lucas lanzziano <pachecolanzziano@gmail.com>
2022-05-06 16:35:36 -0500

    Ejercicio 1 dos numero buscar el max

---
+++ Eje1_max.py
@@ -0,0 +1,15 @@
+#  Definir una función max() que tome como argumento dos números y devuelva el mayor de ellos.
+def maximo(number1, number2):
+    if number1 > number2:
+        bigger_number = number1
+    else:
+        bigger_number = number2
+    return bigger_number
+
+def run():
+    number1 = int(input('Number 1:'))
+    number2 = int(input('Number 2:'))
+    print(f"El numero mayor es: "+str(maximo(number1, number2)))
+
+if __name__ == "__main__":
+    run()
