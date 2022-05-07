34d288a5f415215b97c55b12121c25263ae991ac
lucas lanzziano <pachecolanzziano@gmail.com>
2022-05-07 12:10:20 -0500

    7-05-22

--- Eje3_longitud_de_lista_cadena.py
+++ Eje3_longitud_de_lista_cadena.py
@@ -1,12 +1,12 @@
 #Definir una función que calcule la longitud de una lista o una cadena dada. (Es cierto que python tiene la función len() incorporada, pero escribirla por nosotros mismos resulta un muy buen ejercicio.
 def calcular_longitud(palabra):
-    # i=0
-    for i in palabra:
+    i=0
+    for n in palabra:
         i+=1
     return i
 
 def run():
-    palabra = input('Ingrese la palabra:')
+    palabra = str(input('Ingrese la palabra:'))
     print("La palabra tiene "+str(calcular_longitud(palabra))+" elementos")
 
 if __name__ == "__main__":
